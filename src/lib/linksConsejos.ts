85e0cae099487f4489130986cc1ec0c340932a42
Andres M. Cardenas <andrescar18@gmail.com>
2024-01-09 17:23:55 -0500

    se crean los recusos de links en TS

---
+++ src/lib/linksConsejos.ts
@@ -0,0 +1,31 @@
+/*
+Recursos para la seccion de consejos para iniciar en el mundo de la programacion
+
+### Fazt ###
+https://www.youtube.com/watch?v=xQLqvxezZrA
+
+### Programador X ###
+https://www.youtube.com/watch?v=OhAqBrVWMyc
+https://www.youtube.com/watch?v=MvCbPrrQGJY
+
+
+### DeivChoi ###
+https://www.youtube.com/watch?v=v3NH6kG-O3s
+
+### Soy Dalto ###
+https://www.youtube.com/watch?v=8tW-Yr224-k
+
+### Hola Mundo ###
+https://www.youtube.com/watch?v=OOMCoK5Jb_g
+https://www.youtube.com/watch?v=SmbEWc37z7U
+
+### Victor Robles ###
+https://www.youtube.com/watch?v=eCNnxo7Y7oM
+
+### Midudev ###
+https://www.youtube.com/watch?v=-yw7_3ysx1Q
+
+### CodigoFacilito ###
+https://www.youtube.com/watch?v=vgSXUjvwv_w
+
+*/
---
+++ src/lib/linksGit.ts
@@ -0,0 +1,37 @@
+/*
+Recursos para la seccion de control de versiones de git
+
+### Git ###
+
+https://git-scm.com/  pagina oficial de git
+https://git-scm.com/book/en/v2 libro oficial de git
+
+### MoureDev ###
+https://www.youtube.com/watch?v=3GymExBkKjE
+
+### Fazt ###
+youtube.com/watch?v=HiXLkL42tMU
+
+### Hola Mundo ###
+https://www.youtube.com/watch?v=VdGzPZ31ts8
+
+### Soy Full Stack ###
+https://www.youtube.com/watch?v=XDZ9Gqcr4c4
+
+### FreeCodeCamp ###
+https://www.freecodecamp.org/espanol/news/aprende-git-y-github-curso-desde-cero/
+https://www.youtube.com/watch?v=mBYSUUnMt9M
+
+### Microsoft Learn ###
+https://learn.microsoft.com/es-es/training/modules/intro-to-git/
+
+### Sergie Code ###
+https://www.youtube.com/watch?v=7ylE8cm3mb0
+
+### Uniwebsidad ###
+https://uniwebsidad.com/libros/pro-git
+
+### Learn Git Branching ###
+https://learngitbranching.js.org/
+
+*/
